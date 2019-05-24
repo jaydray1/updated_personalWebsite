7e7052d0fb59f833a20d02c1b0e8ad3874cd1572
jaydray1 <jdreher15@gmail.com>
2019-05-24 05:22:29 -0600

    LinkedIn Hover button added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { IntroCardComponent } from './intro-card/intro-card.component';
+import { RetroTechLinksComponent } from './retro-tech-links/retro-tech-links.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, IntroCardComponent ],
+  declarations: [ AppComponent, HelloComponent, IntroCardComponent, RetroTechLinksComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/retro-tech-links/retro-tech-links.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-retro-tech-links',
+  templateUrl: './retro-tech-links.component.html',
+  styleUrls: ['./retro-tech-links.component.css']
+})
+export class RetroTechLinksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
